b3826f1db301aebe9a9827772d3d5e0c23775909
iamkroot <kroot.patel@gmail.com>
2026-06-19 11:29:50 -0400

    Fix ariang

--- modules/services/aria2.nix
+++ modules/services/aria2.nix
@@ -7,6 +7,23 @@
   ...
 }:
 {
+  # FIXME: Needed to work around https://github.com/NixOS/nixpkgs/issues/523144
+  nixpkgs.overlays = [
+    (final: prev: {
+      ariang = prev.ariang.override {
+        buildNpmPackage = args: prev.buildNpmPackage (args // {
+          version = "1.3.14";
+          src = final.fetchFromGitHub {
+            owner = "timhae";
+            repo = "AriaNg";
+            rev = "7d0538b";
+            hash = "sha256-iUgUT1Vq0KExDT+xSrbvZDDs48GOk+gE6wPAKooFhuU=";
+          };
+          npmDepsHash = "sha256-XHoPPrebNgGZnVQmA0d5OeR+ZWJQEdZU4Ibcbd80/oM=";
+        });
+      };
+    })
+  ];
   imports = [
     (myUtils.mkCaddyModule "aria2" { authelia = false; })
     (myUtils.mkCaddyModule "ariang" {
