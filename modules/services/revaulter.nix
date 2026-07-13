ea3b5b1df68c2770ee12ce644f962a5b74f39a8b
iamkroot <kroot.patel@gmail.com>
2026-07-12 21:41:24 -0700

    Revaulter webhook

--- modules/services/revaulter.nix
+++ modules/services/revaulter.nix
@@ -33,11 +33,17 @@ in
 
   vaultix.secrets."revaulter/secret_key" = { };
   vaultix.secrets."revaulter/session_signing_key" = { };
+  vaultix.secrets."revaulter/webhook_format" = { };
+  vaultix.secrets."revaulter/webhook_key" = { };
+  vaultix.secrets."revaulter/webhook_url" = { };
 
   vaultix.templates."revaulter.env" = {
     content = ''
       REVAULTER_SECRETKEY=${config.vaultix.placeholder."revaulter/secret_key"}
       REVAULTER_SESSIONSIGNINGKEY=${config.vaultix.placeholder."revaulter/session_signing_key"}
+      REVAULTER_WEBHOOKURL=${config.vaultix.placeholder."revaulter/webhook_url"}
+      REVAULTER_WEBHOOKKEY=${config.vaultix.placeholder."revaulter/webhook_key"}
+      REVAULTER_WEBHOOKFORMAT=${config.vaultix.placeholder."revaulter/webhook_format"}
     '';
   };
 }
