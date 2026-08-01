7ad74e6d832186720c846a0bb2cbd42f92099931
iamkroot <kroot.patel@gmail.com>
2026-08-01 08:24:15 -0700

    Bump deps

--- modules/services/caddy.nix
+++ modules/services/caddy.nix
@@ -32,7 +32,7 @@
     # Custom Caddy build with Porkbun DNS plugin for DNS-01 ACME
     package = pkgs.caddy.withPlugins {
       plugins = [ "github.com/caddy-dns/porkbun@v0.3.1" ];
-      hash = "sha256-JtzeWz9GdW/+1Qft5nU9diPkFQvPGxQkgR8n8w+ryoI=";
+      hash = "sha256-CjL8dMdnsiawaPiQGRvL3he4Ydd3nIbQs6tBWMwUbaw=";
     };
 
     extraConfig = ''
