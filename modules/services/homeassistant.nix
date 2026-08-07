fe634e3ab0a11f4eba677ec74d488f64864852b6
iamkroot <kroot.patel@gmail.com>
2026-08-07 14:28:26 -0700

    Add hass

---
+++ modules/services/homeassistant.nix
@@ -0,0 +1,35 @@
+{ config, myUtils, ... }:
+let
+  port = config.infra.services.ports.homeassistant;
+in
+{
+  imports = [
+    (myUtils.mkCaddyModule "homeassistant" { authelia = false; })
+  ];
+
+  virtualisation.podman = {
+    enable = true;
+    dockerCompat = true;
+  };
+  virtualisation.oci-containers.backend = "podman";
+
+  virtualisation.oci-containers.containers."homeassistant" = {
+    image = "ghcr.io/home-assistant/home-assistant:stable";
+    ports = [ "127.0.0.1:${toString port}:8123" ];
+    volumes = [
+      "/var/lib/homeassistant:/config"
+      "/etc/localtime:/etc/localtime:ro"
+    ];
+    environment = {
+      TZ = "America/Los_Angeles";
+    };
+    extraOptions = [
+      "--health-interval=5m"
+      "--health-retries=3"
+    ];
+  };
+
+  systemd.tmpfiles.rules = [
+    "d /var/lib/homeassistant 0755 root root -"
+  ];
+}
