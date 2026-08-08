f700bb99d6b9fc3c4dc99cf8b28a470f8317fb28
iamkroot <kroot.patel@gmail.com>
2026-08-08 08:28:26 -0700

    Fix nfs

--- modules/storage/nfs.nix
+++ modules/storage/nfs.nix
@@ -1,15 +1,38 @@
 {
-  config,
   pii,
   hostKey,
   ...
 }:
+let
+  ip = pii.hosts.${hostKey}.localIp;
+in
 {
   services.nfs.server.enable = true;
 
+  fileSystems."/export/media" = {
+    device = pii.storage.media_main.mountpoint;
+    fsType = "none";
+    options = [
+      "bind"
+      "nofail"
+    ];
+    depends = [ pii.storage.media_main.mountpoint ];
+  };
+
+  fileSystems."/export/data" = {
+    device = pii.storage.data_main.mountpoint;
+    fsType = "none";
+    options = [
+      "bind"
+      "nofail"
+    ];
+    depends = [ pii.storage.data_main.mountpoint ];
+  };
+
   services.nfs.server.exports = ''
-    ${pii.storage.media_main.mountpoint}  ${pii.hosts.${hostKey}.localIp}/24(rw,sync,no_subtree_check,no_root_squash)
-    ${pii.storage.data_main.mountpoint}  ${pii.hosts.${hostKey}.localIp}/24(rw,sync,no_subtree_check,no_root_squash)
+    /export        ${ip}/24(rw,fsid=0,no_subtree_check,crossmnt)
+    /export/media  ${ip}/24(rw,sync,no_subtree_check,no_root_squash,nohide,fsid=1)
+    /export/data   ${ip}/24(rw,sync,no_subtree_check,no_root_squash,nohide,fsid=2)
   '';
 
   networking.firewall.allowedTCPPorts = [ 2049 ];
