b2718ad867b5c4d9dd1ad4964a48fce2f8cae2f8
iamkroot <kroot.patel@gmail.com>
2026-08-16 08:55:05 -0700

    Fix nfs

--- modules/storage/nfs.nix
+++ modules/storage/nfs.nix
@@ -1,10 +1,12 @@
 {
+  config,
   pii,
   hostKey,
   ...
 }:
 let
   ip = pii.hosts.${hostKey}.localIp;
+  meshNet = pii.networks.mesh.subnet;
 in
 {
   services.nfs.server.enable = true;
@@ -13,27 +15,44 @@ in
     device = pii.storage.media_main.mountpoint;
     fsType = "none";
     options = [
-      "bind"
+      "rbind"
       "nofail"
+      "noauto"
+      "x-systemd.requires=media-apps.target"
+      "x-systemd.after=media-apps.target"
+      "x-systemd.wanted-by=media-apps.target"
+      "x-systemd.binds-to=media-apps.target"
     ];
-    depends = [ pii.storage.media_main.mountpoint ];
   };
 
   fileSystems."/export/data" = {
     device = pii.storage.data_main.mountpoint;
     fsType = "none";
     options = [
-      "bind"
+      "rbind"
       "nofail"
+      "noauto"
+      "x-systemd.requires=media-apps.target"
+      "x-systemd.after=media-apps.target"
+      "x-systemd.wanted-by=media-apps.target"
+      "x-systemd.binds-to=media-apps.target"
     ];
-    depends = [ pii.storage.data_main.mountpoint ];
+  };
+
+  systemd.targets.media-apps.wants = [
+    "export-media.mount"
+    "export-data.mount"
+  ];
+
+  systemd.services.nfs-server = {
+    after = [ "media-apps.target" ];
   };
 
   services.nfs.server.exports = ''
-    /export        ${ip}/24(rw,fsid=0,no_subtree_check,crossmnt)
-    /export/media  ${ip}/24(rw,sync,no_subtree_check,no_root_squash,nohide,fsid=1)
-    /export/data   ${ip}/24(rw,sync,no_subtree_check,no_root_squash,nohide,fsid=2)
+    /export        ${ip}/24(rw,fsid=0,no_subtree_check,crossmnt) ${meshNet}(rw,fsid=0,no_subtree_check,crossmnt)
+    /export/media  ${ip}/24(rw,async,no_subtree_check,no_root_squash,nohide,crossmnt,fsid=1) ${meshNet}(rw,async,no_subtree_check,no_root_squash,nohide,crossmnt,fsid=1)
+    /export/data   ${ip}/24(rw,async,no_subtree_check,no_root_squash,nohide,crossmnt,fsid=2) ${meshNet}(rw,async,no_subtree_check,no_root_squash,nohide,crossmnt,fsid=2)
   '';
 
-  networking.firewall.allowedTCPPorts = [ 2049 ];
+  networking.firewall.allowedTCPPorts = [ config.infra.services.ports.nfs ];
 }
